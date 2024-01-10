edbdc644465400fafb509070795c5c8fa07e76c2
Hugoll-1 <hugolin.rouyer9@etu.univ-lorraine.fr>
2024-01-10 17:39:13 +0100

    Ajout de JavaDoc

--- src/main/java/com/overcooked/ptut/objet/transformateur/Planche.java
+++ src/main/java/com/overcooked/ptut/objet/transformateur/Planche.java
@@ -3,11 +3,21 @@ package com.overcooked.ptut.objet.transformateur;
 import com.overcooked.ptut.recettes.etat.Coupe;
 
 public class Planche extends Transformateur{
+
+    /**
+     * Constructeur de la classe Planche
+     * @param x
+     * @param y
+     */
     public Planche(int x, int y) {
         super(x, y);
         etat = new Coupe(elemPose);
     }
 
+    /**
+     * Constructeur par copie de la classe Planche
+     * @param planche
+     */
     public Planche(Planche planche) {
         super(planche);
     }
--- src/main/java/com/overcooked/ptut/objet/transformateur/Poele.java
+++ src/main/java/com/overcooked/ptut/objet/transformateur/Poele.java
@@ -3,11 +3,21 @@ package com.overcooked.ptut.objet.transformateur;
 import com.overcooked.ptut.recettes.etat.Cuisson;
 
 public class Poele extends Transformateur {
+
+    /**
+     * Constructeur de la classe Poele
+     * @param x
+     * @param y
+     */
     public Poele(int x, int y) {
         super(x, y);
         etat = new Cuisson(elemPose);
     }
 
+    /**
+     * Constructeur par copie de la classe Poele
+     * @param poele
+     */
     public Poele(Poele poele) {
         super(poele);
     }
