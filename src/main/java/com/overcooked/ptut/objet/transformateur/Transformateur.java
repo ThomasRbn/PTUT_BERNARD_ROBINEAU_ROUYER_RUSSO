38b2e07a068ed3d894d129c51dbf88deff7a63e8
Azuras03 <nr03russo@gmail.com>
2024-01-10 16:49:01 +0100

    Mise à jour de la Javadoc de la classe Transformateur.java

--- src/main/java/com/overcooked/ptut/objet/transformateur/Transformateur.java
+++ src/main/java/com/overcooked/ptut/objet/transformateur/Transformateur.java
@@ -7,19 +7,39 @@ import com.overcooked.ptut.recettes.etat.Cuisson;
 import com.overcooked.ptut.recettes.etat.Etat;
 
 public abstract class Transformateur extends Bloc {
+
+    /**
+     * Etat de l'aliment (en fonction du transformateur, cela peut être une cuisson, une coupe, etc.)
+     */
     protected Etat etat;
+    /**
+     * Element posé sur le transformateur (Plat)
+     */
     protected Plat elemPose;
 
+    /**
+     * Constructeur d'un transformateur
+     * @param x
+     * @param y
+     */
     public Transformateur(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * Constructeur par copie d'un transformateur
+     * @param t
+     */
     public Transformateur(Transformateur t) {
         super(t);
         this.etat = t.etat;
         this.elemPose = t.elemPose;
     }
 
+    /**
+     * Méthode permettant de transformer un aliment avec l'état auquel il doit être transformé
+     * @return Plat transformé
+     */
     public Plat transform(){
         if(elemPose == null) return null;
         if (elemPose.getRecettesComposees().getFirst().equals(etat)) return elemPose;
@@ -29,13 +49,22 @@ public abstract class Transformateur extends Bloc {
         return elemPose;
     }
 
+    /**
+     * Méthode permettant d'ajouter un Plat sur le transformateur. Le plat doit avoir une seule recette composée.
+     * @param elem Plat à ajouter
+     * @return true si le plat a été ajouté, false sinon
+     */
     public boolean ajouterElem(Plat elem){
         if (elem.getRecettesComposees().size() != 1) return false;
         elemPose = elem;
         return true;
     }
 
-    public void retirerElem(){
+    /**
+     * Méthode permettant de retirer un Plat du transformateur
+     */
+    public Plat retirerElem(){
         elemPose = null;
+        return elemPose;
     }
 }
