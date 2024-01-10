2dbb4ca204a7e460a185bdb0c9e8c533ce94c853
Hugoll-1 <hugolin.rouyer9@etu.univ-lorraine.fr>
2024-01-10 16:47:17 +0100

    Ajout de la methode viderDepot + javadoc

--- src/main/java/com/overcooked/ptut/entites/Depot.java
+++ src/main/java/com/overcooked/ptut/entites/Depot.java
@@ -11,21 +11,46 @@ public class Depot extends Bloc {
 
     private List<Plat> platsDeposes;
 
+    /**
+     * Constructeur de la classe Depot
+     * @param x
+     * @param y
+     */
     public Depot(int x, int y) {
         super(x, y);
         platsDeposes = new ArrayList<>();
     }
 
+    /**
+     * Constructeur par copie de la classe Depot
+     * @param depot
+     */
     public Depot(Depot depot) {
         super(depot);
         this.platsDeposes = new ArrayList<>(depot.platsDeposes);
     }
 
+    /**
+     * Méthode permettant de déposer un plat dans le dépôt
+     * @param plat
+     */
     public void deposerPlat(Plat plat) {
         platsDeposes.add(plat);
     }
 
+    /**
+     * Retourne les plats présent dans le dépôt
+     * @return
+     */
     public List<Plat> getPlatsDeposes() {
         return platsDeposes;
     }
+
+    /**
+     * Vide le dépôt
+     */
+    public void viderDepot() {
+        platsDeposes.clear();
+    }
+
 }
