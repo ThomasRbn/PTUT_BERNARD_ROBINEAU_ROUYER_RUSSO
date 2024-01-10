5f8c3b930680ffd30759a30d8a576be15641618e
Hugoll-1 <hugolin.rouyer9@etu.univ-lorraine.fr>
2024-01-10 16:47:55 +0100

    Ajout du constructeur par copie de PlanDeTravail + javadoc

--- src/main/java/com/overcooked/ptut/entites/PlanDeTravail.java
+++ src/main/java/com/overcooked/ptut/entites/PlanDeTravail.java
@@ -7,20 +7,46 @@ public class PlanDeTravail extends Bloc {
 
     Plat inventaire;
 
+    /**
+     * Constructeur de la classe PlanDeTravail
+     * @param x
+     * @param y
+     */
     public PlanDeTravail(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * Constructeur par copie de la classe PlanDeTravail
+     * @param planDeTravail
+     */
+    public PlanDeTravail(PlanDeTravail planDeTravail) {
+        super(planDeTravail);
+        this.inventaire = planDeTravail.inventaire;
+    }
+
+    /**
+     * Méthode permettant de poser un plat sur le plan de travail
+     * @param plat
+     */
     public void poserDessus(Plat plat) {
         inventaire = plat;
     }
 
+    /**
+     * Méthode permettant de prendre un plat sur le plan de travail
+     * @return
+     */
     public Plat prendre() {
         Plat plat = inventaire;
         inventaire = null;
         return plat;
     }
 
+    /**
+     * Retourne le plat présent sur le plan de travail
+     * @return
+     */
     public Plat getInventaire() {
         return inventaire;
     }
