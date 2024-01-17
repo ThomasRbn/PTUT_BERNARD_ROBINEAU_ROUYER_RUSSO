f5fa3279d1b504b9cfa3da376f5ae46bd22be2b2
Azuras03 <nr03russo@gmail.com>
2024-01-17 16:49:08 +0100

    Refactor de Plat avec une nouvelle Structure (TreeMap). Cela évite la duplication d'Aliments

--- src/main/java/com/overcooked/ptut/recettes/aliment/Plat.java
+++ src/main/java/com/overcooked/ptut/recettes/aliment/Plat.java
@@ -1,72 +1,112 @@
 package com.overcooked.ptut.recettes.aliment;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Classe abstraite representant un plat, avec un nom et une liste de recettes qui le composent
  */
 public class Plat extends Aliment {
     // Liste des recettes qui composent la recette en question
-    List<Aliment> recettesComposees;
+    Map<String, Aliment> recettesComposees;
 
     public Plat() {
         super();
-        this.nom = "Plat";
-        description = "Plat";
-        this.recettesComposees = new ArrayList<>();
+        this.nom = "";
+        this.recettesComposees = new TreeMap<>();
+    }
+
+    public String getNomPlat() {
+        String nomPlat = "";
+        for (String aliment : recettesComposees.keySet()) {
+            nomPlat += aliment;
+        }
+        return nomPlat;
     }
 
     public Plat(Plat plat){
         super();
-        this.nom = "Plat";
-        description = "Plat";
-        this.recettesComposees = new ArrayList<>(plat.getRecettesComposees());
+        this.nom = plat.nom;
+        this.recettesComposees = new TreeMap<>(plat.getRecettesComposeesMap());
     }
 
     public Plat(Plat plat1, Plat plat2){
         super();
-        this.nom = "Plat";
-        description = "Plat";
-        this.recettesComposees = new ArrayList<>();
-        this.recettesComposees.addAll(plat1.getRecettesComposees());
-        this.recettesComposees.addAll(plat2.getRecettesComposees());
+        this.recettesComposees = new TreeMap<>();
+        this.recettesComposees.putAll(plat1.getRecettesComposeesMap());
+        this.recettesComposees.putAll(plat2.getRecettesComposeesMap());
+        this.nom = getNomPlat();
     }
 
     public Plat(Aliment aliment1, Aliment aliment2) {
         super();
-        this.nom = "Plat";
-        description = "Plat";
-        this.recettesComposees = new ArrayList<>();
-        this.recettesComposees.add(aliment1.cloneAlim());
-        this.recettesComposees.add(aliment2.cloneAlim());
-    }
-
-    public void fusionnerNomsPlats(Plat plat1, Plat plat2){
-        this.nom = plat1.nom + " " + plat2.nom;
-    }
-
-    public void fusionnerDescriptionsPlats(Plat plat1, Plat plat2){
-        this.description = plat1.description + " " + plat2.description;
-    }
-
-    public void fusionnerNomsAliments(Aliment aliment1, Aliment aliment2){
-        this.nom = aliment1.nom + " " + aliment2.nom;
+        this.recettesComposees = new TreeMap<>();
+        this.recettesComposees.put(aliment1.getNom(), aliment1.cloneAlim());
+        this.recettesComposees.put(aliment2.getNom(), aliment2.cloneAlim());
+        this.nom = getNomPlat();
     }
 
     public Plat(Aliment aliment){
         super();
         this.nom = "Plat";
-        description = "Plat";
-        this.recettesComposees = new ArrayList<>();
-        this.recettesComposees.add(aliment.cloneAlim());
+        this.recettesComposees = new TreeMap<>();
+        this.recettesComposees.put(aliment.getNom(), aliment.cloneAlim());
     }
 
-    public boolean equals(Plat plat) {
+    public void ajouterAliment(Aliment aliment) {
+        String nomAlim = aliment.getSimplifiedNom();
+        recettesComposees.put(nomAlim, aliment);
+        this.nom = getNomPlat();
+    }
+
+    public void fusionerPlat(Plat plat){
+        this.recettesComposees.putAll(plat.recettesComposees);
+        this.nom = getNomPlat();
+    }
+
+    public boolean estFusionnable(Plat plat){
+        //On vérifie qu'il n'y a aucun aliment en commun dans chaque plat
+        for (Aliment aliment : plat.recettesComposees.values()){
+            if (recettesComposees.containsValue(aliment)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Aliment> getRecettesComposees() {
+        List<Aliment> recettesComposees = new ArrayList<>();
+        for (Aliment aliment : this.recettesComposees.values()) {
+            recettesComposees.add(aliment.cloneAlim());
+        }
+        return recettesComposees;
+    }
+
+    public Map<String, Aliment> getRecettesComposeesMap() {
+        Map<String, Aliment> recettesComposees = new TreeMap<>();
+        for (Aliment aliment : this.recettesComposees.values()) {
+            recettesComposees.put(aliment.getNom(), aliment.cloneAlim());
+        }
+        return recettesComposees;
+    }
+
+    public void viderAliments(){
+        recettesComposees.clear();
+        this.nom = getNomPlat();
+    }
+
+    @Override
+    public String toString() {
+        return getNomPlat();
+    }
+
+    public boolean equals(Object o) {
+        Plat plat = (Plat) o;
+        System.out.println(plat);
+        System.out.println(this);
         if (this == plat) return true;
         if (plat == null || getClass() != plat.getClass()) return false;
-        List<Aliment> currAliments = new ArrayList<>(recettesComposees);
-        for (Aliment aliment : plat.recettesComposees) {
+        List<Aliment> currAliments = new ArrayList<>(recettesComposees.values());
+        for (Aliment aliment : plat.recettesComposees.values()) {
             int currIndex = currAliments.indexOf(aliment);
             if (currIndex != -1) {
                 currAliments.remove(currIndex);
@@ -77,44 +117,8 @@ public class Plat extends Aliment {
         return currAliments.isEmpty();
     }
 
-    public void ajouterAliment(Aliment aliment) {
-        recettesComposees.add(aliment);
-    }
-
-    public void fusionerPlat(Plat plat){
-        recettesComposees.addAll(plat.recettesComposees);
-    }
-
-    public boolean estFusionnable(Plat plat){
-        //On vérifie qu'il n'y a aucun aliment en commun dans chaque plat
-        for (Aliment aliment : plat.recettesComposees){
-            if (recettesComposees.contains(aliment)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public List<Aliment> getRecettesComposees() {
-        List<Aliment> recettesComposees = new ArrayList<>();
-        for (Aliment aliment : this.recettesComposees) {
-            recettesComposees.add(aliment.cloneAlim());
-        }
-        return recettesComposees;
-    }
-
-    public boolean retirerAliment(Aliment aliment) {
-        return recettesComposees.remove(aliment);
-    }
-
-    public void viderAliments(){
-        recettesComposees.clear();
-    }
-
     @Override
-    public String toString() {
-        return "Plat{" +
-                "recettesComposees=" + recettesComposees +
-                '}';
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), recettesComposees);
     }
 }
