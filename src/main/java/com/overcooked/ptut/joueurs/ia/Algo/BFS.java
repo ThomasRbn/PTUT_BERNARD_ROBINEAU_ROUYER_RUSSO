f9759c0133800c4f00616191d2ea9d9ace9578d5
Azuras03 <nr03russo@gmail.com>
2024-01-30 16:50:35 +0100

    Modifications mineures, l'exécution de l'IA n'est plus compromise par des exceptions

--- src/main/java/com/overcooked/ptut/joueurs/ia/Algo/BFS.java
+++ src/main/java/com/overcooked/ptut/joueurs/ia/Algo/BFS.java
@@ -42,7 +42,7 @@ public class BFS extends TreeSearchAC {
         if (ArgParse.DEBUG)
             System.out.print("[" + state);
 
-        while (!frontier.isEmpty()){
+        while (!frontier.isEmpty() && (explored.size() < 500)){
             // Stratégie: BFS
             node = frontier.poll();
 
